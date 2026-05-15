df9a4c3a1d259e8c1361a9ddc05a703026cfc9bb
Cast43 <caiodcastilho@gmail.com>
2026-05-14 21:18:02 -0300

    feat:pause when upgrades pop up

--- Assets/Script/Systems/ArrowSystem.cs
+++ Assets/Script/Systems/ArrowSystem.cs
@@ -7,7 +7,8 @@ using Unity.NetCode;
 using UnityEngine;
 using Unity.Collections;
 
-[UpdateInGroup(typeof(PausablePredictedGroup))]
+// Movido para PredictedSimulationSystemGroup para rodar durante o pause e poder zerar a velocidade
+[UpdateInGroup(typeof(PredictedSimulationSystemGroup))]
 partial struct ArrowSystem : ISystem
 {
     [BurstCompile]
@@ -19,45 +20,27 @@ partial struct ArrowSystem : ISystem
             isPaused = matchState.IsPaused;
         }
 
-        BeginSimulationEntityCommandBufferSystem.Singleton ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
-        EntityCommandBuffer entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
-
-        // Obtenha o PhysicsWorldSingleton para acessar o CollisionWorld.
-        PhysicsWorldSingleton physicsWorldSingleton = SystemAPI.GetSingleton<PhysicsWorldSingleton>();
-        CollisionWorld collisionWorld = physicsWorldSingleton.CollisionWorld;
-
-        foreach (var (physicsVelocity, arrow, direction, localTransform, arrowEntity) in
-                SystemAPI.Query<RefRW<PhysicsVelocity>, RefRO<Arrow>, RefRO<Direction>, RefRO<LocalTransform>>().WithAll<Simulate>().WithEntityAccess())
+        state.Dependency = new ArrowMoveJob
         {
-            if (isPaused)
-            {
-                physicsVelocity.ValueRW.Linear = float3.zero;
-                physicsVelocity.ValueRW.Angular = float3.zero;
-            }
-            else
-            {
-                physicsVelocity.ValueRW.Linear = direction.ValueRO.lookDirection * arrow.ValueRO.moveSpeed;
-                physicsVelocity.ValueRW.Angular = float3.zero;
-            }
-        }
-
-        // state.Dependency = new ArrowJob
-        // {
-        //     transformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true),
-        // }.ScheduleParallel(state.Dependency);
+            isPaused = isPaused
+        }.ScheduleParallel(state.Dependency);
     }
 }
 
 [BurstCompile]
 [WithAll(typeof(Simulate))]
-public partial struct ArrowJob : IJobEntity
+public partial struct ArrowMoveJob : IJobEntity
 {
-    [ReadOnly] public ComponentLookup<LocalTransform> transformLookup;
+    [ReadOnly] public bool isPaused;
 
-    [BurstCompile]
-    public void Execute(ref PhysicsVelocity physicsVelocity, ref Arrow arrow, ref Direction direction, Entity arrowEntity, [ChunkIndexInQuery] int sortKey)
+    public void Execute(ref PhysicsVelocity physicsVelocity, in Arrow arrow, in Direction direction)
     {
-        if (!transformLookup.HasComponent(arrowEntity)) return;
+        if (isPaused)
+        {
+            physicsVelocity.Linear = float3.zero;
+            physicsVelocity.Angular = float3.zero;
+            return;
+        }
 
         physicsVelocity.Linear = direction.lookDirection * arrow.moveSpeed;
         physicsVelocity.Angular = float3.zero;
